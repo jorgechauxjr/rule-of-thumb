ef039e2cc3658c00b1bf00e3648fc455c92cbe23
jorge <chauxjorge@hotmail.com>
2026-01-16 18:36:59 -0500

    [Add] Initial files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainHeader from './components/MainHeader.vue'
+import MainAside from './components/MainAside.vue'
+import SecondaryAside from './components/SecondaryAside.vue'
+import MainFooter from './components/MainFooter.vue'
+</script>
+
+<template>
+  <MainHeader />
+  <MainAside />
+  <SecondaryAside />
+  <MainFooter />
+</template>
+
+<style scoped></style>
