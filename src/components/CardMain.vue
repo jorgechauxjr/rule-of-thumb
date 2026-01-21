d4b312d5104dad22f38794977e973270f7c3b495
jorge <chauxjorge@hotmail.com>
2026-01-21 09:50:40 -0500

    [Add] Card structure with dynamic information

--- src/components/CardMain.vue
+++ src/components/CardMain.vue
@@ -1,17 +1,32 @@
 <script setup>
-import img1 from '@/assets/img/elon.png'
+import thumbsUp from '@/assets/img/thumbs-up.svg'
+import thumbsDown from '@/assets/img/thumbs-down.svg'
+
+const props = defineProps({
+  picture: String,
+  name: String,
+  description: String,
+  category: String,
+  lastUpdated: String,
+})
+console.log('Name: ', props.name)
 </script>
 
 <template>
   <div class="card">
     <div class="card-body">
-      <img :src="img1" alt="" class="img-fluid" />
-      <h4 class="card-title">My title</h4>
-      <p class="card-text">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nostrum ut, quam consequuntur
-        accusantium id deleniti officiis quo rem! Soluta provident et pariatur tenetur ipsum.
-        Nesciunt aut sed omnis corrupti!
-      </p>
+      <img :src="picture" alt="" class="img-fluid" />
+      <img src="" alt="" />
+      <h4 class="card-title">{{ name }}</h4>
+      <p class="card-text">{{ description }}</p>
+      <p>Last updated: {{ lastUpdated }} in {{ category }}</p>
+      <button class="btn btn-success">
+        <img :src="thumbsUp" alt="thumbs up" />
+      </button>
+      <button class="btn btn-danger">
+        <img :src="thumbsDown" alt="thumbs down" />
+      </button>
+      <button class="btn btn-light">VOTE NOW</button>
     </div>
   </div>
 </template>
--- src/components/CardsMain.vue
+++ src/components/CardsMain.vue
@@ -1,11 +1,28 @@
 <script setup>
 import CardMain from './CardMain.vue'
+import info from '@/assets/data.json'
+import im1 from '../assets/img/kanye.png'
+import im2 from '../assets/img/mark.png'
+import im3 from '../assets/img/cristina.png'
+import im4 from '../assets/img/malala.png'
+import im5 from '../assets/img/elon.png'
+import im6 from '../assets/img/greta.png'
+
+const images = [im1, im2, im3, im4, im5, im6]
+
+const persons = info.data
 </script>
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-4">
-        <CardMain />
+      <div class="col-md-4" v-for="(person, index) in persons" :key="index">
+        <CardMain
+          :name="person.name"
+          :picture="images[index]"
+          :description="person.description"
+          :category="person.category"
+          :last-updated="person.lastUpdated"
+        />
       </div>
     </div>
   </div>
